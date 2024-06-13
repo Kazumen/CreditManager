c0fa7b4346868721e91f9c0b490319e5e7fbd7b4
Witold Spelina <witalikspelina@gmail.com>
2024-06-13 18:44:06 +0300

    Mnogo

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -2,6 +2,12 @@ import {Component} from '@angular/core';
 import {CreditRecord} from "../shared/models/credit-record";
 
 const data: CreditRecord[] = [
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
+  {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'},
   {date: '2020', name: 'Witold', bank: "Privat", sum: 2000, percent: 4, term: '2022'}
 ]
 
